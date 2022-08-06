39b916584652bb46af9aad0de761d4002a8a8c2d
piero2023 <estibensmax2022@gmail.com>
2022-08-06 16:11:54 -0700

    correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name \
-            LIKE BYNARY '{}' ORDER BY id".format(sys.argv[4]))
+    cur.execute('SELECT * FROM states WHERE name \
+            LIKE BYNARY '{}' ORDER BY id'.format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
