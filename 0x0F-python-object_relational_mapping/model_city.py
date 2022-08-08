a7cab5314ed96f098fd7a38a99645fb7aad1866a
piero2023 <estibensmax2022@gmail.com>
2022-08-07 17:16:15 -0700

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-Cities in state
-contains the class City
-"""
-from sqlalchemy import Comlumn, Integer, String, ForeignKey
+""" This script lists all State objects with the name passed as
+argument from database hbtn_0e_6_usa """
+
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """ contains the class definition of a City """
+    """
+    is the class City inherits from Base
+    """
     __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True,
+                unique=True, nullable=False, primary_key=True)
 
+    name = Column(String(128), nullable=False)
 
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
-    name = Column(String(128), nullble=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
